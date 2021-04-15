56e124f58e7c12662988a4d7b033653fd1ffec4c
Der Meyer <simon.der.meyer@gmail.com>
2021-04-15 16:42:49 +0200

    copy current wp site for hosting test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Studio Anderhalden</h1>
+            <p>Independent design studio für Gebrauchsgrafik. We offer solutions within the range of visual communication für lokale and (theoretisch) international clients.</p>
+            <h2>Basis</h2>
+            <p>Studio Anderhalden<br/>Bahnhofstrasse 15<br/>6072 Sachseln<br/>Switzerland</p>
+            <h2>Kontakt</h2>
+            <a href="mailto:mail@studioanderhalden.ch">mail@studioanderhalden.ch</a>
+        </div>
+    );
 }
 
 export default App;
