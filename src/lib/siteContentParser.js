be7dda049a054953e5484b77ddbe62d6cf681d2a
Der Meyer <simon.der.meyer@gmail.com>
2021-04-23 11:46:08 +0200

    project parser

--- src/lib/siteContentParser.js
+++ src/lib/siteContentParser.js
@@ -62,13 +62,78 @@ function siteContentParser(raw) {
     return raw;
 }
 
-function projectParser(item) {
-    return item;
+// entry parsers
+
+function parseRichText(entry) {
+    if (
+        !isObject(entry)
+        || !entry.hasOwnProperty('content')
+        || !entry.hasOwnProperty('nodeType')
+    ) {
+        console.warn('unknown rich text entry', entry);
+        return;
+    }
+    return entry;
 }
+
+function parseImage(entry) {
+    if (
+        !isObject(entry)
+        || !isObject(entry.sys)
+        || !isObject(entry.fields)
+    ) {
+        console.warn('unknown image entry', entry);
+        return;
+    }
+    return {
+        id: entry.sys.id,
+        title: entry.fields.title,
+        url: isObject(entry.fields.file) ? entry.fields.file.url : ''
+    }
+}
+
+// project parser
+
+function projectParser(item) {
+    if (!isObject(item.fields)) {
+        console.warn('unknown project item', item);
+        return;
+    }
+    const project = {
+        title: '',
+        text: null,
+        footnote: null,
+        thumbnail: null,
+        images: []
+    };
+    if (typeof item.fields.titel === typeof project.title) {
+        project.title = item.fields.titel;
+    }
+    const text = parseRichText(item.fields.text);
+    if (text) {
+        project.text = text;
+    }
+    const footnote = parseRichText(item.fields.fussnote);
+    if (footnote) {
+        project.footnote = footnote;
+    }
+    const thumbnail = parseImage(item.fields.thumbnail);
+    if (thumbnail) {
+        project.thumbnail = thumbnail;
+    }
+    if (Array.isArray(item.fields.bilder)) {
+        project.images = item.fields.bilder.map(entry => parseImage(entry)).filter(e => !!e);
+    }
+    return project;
+}
+
+// info block parser
 
 function infoBlockParser(item) {
     return item;
 }
+
+// config parser
 
 const minAnimationTime = 0.2;
 const maxAnimationTime = 4;
@@ -78,41 +143,45 @@ const minImageRatio = 1;
 const maxImageRatio = 2;
 
 function configParser(item) {
-    const result = {
-        palette: {}
-    };
     if (!isObject(item.fields)) {
         console.warn('unknown config item', item);
         return;
     }
-    if (typeof item.fields.websiteTitel === 'string') {
-        result.documentTitle = item.fields.websiteTitel;
+    const config = {
+        documentTitle: '',
+        animationTime: 0,
+        imageSize: 0,
+        ratio: 0,
+        palette: {}
+    };
+    if (typeof item.fields.websiteTitel === typeof config.documentTitle) {
+        config.documentTitle = item.fields.websiteTitel;
     }
-    if (isHexColor(item.fields.schwarz)) {
-        result.palette.black = item.fields.schwarz;
+    if (typeof item.fields.animationszeit === typeof config.animationTime) {
+        config.animationTime = Math.min(maxAnimationTime, Math.max(minAnimationTime, item.fields.animationszeit));
     }
-    if (isHexColor(item.fields.grau)) {
-        result.palette.grey = item.fields.grau;
-    }
-    if (isHexColor(item.fields.hellgrau)) {
-        result.palette.lightgrey = item.fields.hellgrau;
-    }
-    if (isHexColor(item.fields.weiss)) {
-        result.palette.white = item.fields.weiss;
-    }
-    if (typeof item.fields.animationszeit === 'number') {
-        result.animationTime = Math.min(maxAnimationTime, Math.max(minAnimationTime, item.fields.animationszeit));
-    }
-    if (typeof item.fields.maximaleBildgre === 'number') {
-        result.imageSize = Math.min(maxImageSize, Math.max(minImageSize, item.fields.maximaleBildgre));
+    if (typeof item.fields.maximaleBildgre === typeof config.imageSize) {
+        config.imageSize = Math.min(maxImageSize, Math.max(minImageSize, item.fields.maximaleBildgre));
     }
     if (typeof item.fields.bildSeitenverhltnis === 'string') {
         const [ counter, denominator ] = item.fields.bildSeitenverhltnis.split(':').map(e => Number(e));
         if (!isNaN(counter) && !isNaN(denominator)) {
-            result.ratio = Math.min(maxImageRatio, Math.max(minImageRatio, counter / denominator));
+            config.ratio = Math.min(maxImageRatio, Math.max(minImageRatio, counter / denominator));
         }
     }
-    return result;
+    if (isHexColor(item.fields.schwarz)) {
+        config.palette.black = item.fields.schwarz;
+    }
+    if (isHexColor(item.fields.grau)) {
+        config.palette.grey = item.fields.grau;
+    }
+    if (isHexColor(item.fields.hellgrau)) {
+        config.palette.lightgrey = item.fields.hellgrau;
+    }
+    if (isHexColor(item.fields.weiss)) {
+        config.palette.white = item.fields.weiss;
+    }
+    return config;
 }
 
 export default siteContentParser;
